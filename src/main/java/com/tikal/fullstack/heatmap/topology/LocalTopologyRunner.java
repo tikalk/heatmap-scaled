67be17cdd18d23e1c56ecb042e696aa297c4e127
Yanai Franchi <yanai@tikalk.com>
2014-11-21 17:50:21 +0200

    Remove Solr

--- src/main/java/com/tikal/fullstack/heatmap/topology/LocalTopologyRunner.java
+++ src/main/java/com/tikal/fullstack/heatmap/topology/LocalTopologyRunner.java
@@ -12,28 +12,27 @@ import backtype.storm.tuple.Fields;
 
 import com.tikal.fullstack.heatmap.topology.bolts.GeocodeLookupBolt;
 import com.tikal.fullstack.heatmap.topology.bolts.HeatMapBuilderBolt;
-import com.tikal.fullstack.heatmap.topology.bolts.IndexerBolt;
 import com.tikal.fullstack.heatmap.topology.bolts.KafkaOutputBolt;
 import com.tikal.fullstack.heatmap.topology.bolts.PersistorBolt;
 
 public class LocalTopologyRunner {
 	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(LocalTopologyRunner.class);
-	public static void main(String[] args) {
-		int intervalWindow = 2;
-		int emitInterval = 3;
+	public static void main(final String[] args) {
+		final int intervalWindow = 2;
+		final int emitInterval = 3;
 		
-		String solrAddress = "http://localhost:8080";
-		String zkHosts = "localhost";
-		String kafkaTopicName="checkinsTopic";
+		final String solrAddress = "http://localhost:8080";
+		final String zkHosts = "localhost";
+		final String kafkaTopicName="checkinsTopic";
 
-		TopologyBuilder builder = buildTopolgy(kafkaTopicName,zkHosts,intervalWindow, emitInterval, solrAddress);
+		final TopologyBuilder builder = buildTopolgy(kafkaTopicName,zkHosts,intervalWindow, emitInterval, solrAddress);
 
 
-		Config config = new Config();
+		final Config config = new Config();
 		config.setDebug(false);
 		// config.put(Config.TOPOLOGY_TRIDENT_BATCH_EMIT_INTERVAL_MILLIS, 2000);
 
-		LocalCluster localCluster = new LocalCluster();
+		final LocalCluster localCluster = new LocalCluster();
 		localCluster.submitTopology("local-heatmap", config, builder.createTopology());
 		logger.info("##############Topology submitted################");
 		
@@ -44,16 +43,16 @@ public class LocalTopologyRunner {
 
 	}
 
-	private static TopologyBuilder buildTopolgy(String kafkaTopicName,String zkHosts,int intervalWindow, int emitInterval, String solrAddress) {
+	private static TopologyBuilder buildTopolgy(final String kafkaTopicName,final String zkHosts,final int intervalWindow, final int emitInterval, final String solrAddress) {
 		
-		SpoutConfig kafkaConfig = new SpoutConfig(new ZkHosts(zkHosts), kafkaTopicName, "", "storm");
+		final SpoutConfig kafkaConfig = new SpoutConfig(new ZkHosts(zkHosts), kafkaTopicName, "", "storm");
 		kafkaConfig.scheme = new SchemeAsMultiScheme(new StringScheme());
 
-		Config heatmapConfig = new Config();
+		final Config heatmapConfig = new Config();
 		heatmapConfig.put(Config.TOPOLOGY_TICK_TUPLE_FREQ_SECS, emitInterval);
 		heatmapConfig.put("interval-window", intervalWindow);
 		
-		TopologyBuilder builder = new TopologyBuilder();
+		final TopologyBuilder builder = new TopologyBuilder();
 
 //		builder.setSpout("checkins", new Checkins());
 		builder.setSpout("checkins", new KafkaSpout(kafkaConfig), 4);
@@ -64,7 +63,7 @@ public class LocalTopologyRunner {
 		builder.setBolt("persistor", new PersistorBolt(), 2).setNumTasks(4).shuffleGrouping("heatmap-builder");
 		 builder.setBolt("kafkaProducer", new KafkaOutputBolt("localhost:9092","kafka.serializer.StringEncoder","locations-topic"), 2).setNumTasks(4).shuffleGrouping("persistor");
 		
-		builder.setBolt("indexer", new IndexerBolt(solrAddress), 1).setNumTasks(4).shuffleGrouping("persistor");
+//		builder.setBolt("indexer", new IndexerBolt(solrAddress), 1).setNumTasks(4).shuffleGrouping("persistor");
 		return builder;
 	}
 }
--- src/main/java/com/tikal/fullstack/heatmap/topology/bolts/GeocodeLookupBolt.java
+++ src/main/java/com/tikal/fullstack/heatmap/topology/bolts/GeocodeLookupBolt.java
@@ -19,27 +19,33 @@ public class GeocodeLookupBolt extends BaseBasicBolt {
 	private LocatorService locatorService;
 
 	@Override
-	public void declareOutputFields(OutputFieldsDeclarer fieldsDeclarer) {
+	public void declareOutputFields(final OutputFieldsDeclarer fieldsDeclarer) {
 		fieldsDeclarer.declare(new Fields("city","time", "location"));
 	}
 
 	@Override
-	public void prepare(Map stormConf, TopologyContext context) {
+	public void prepare(final Map stormConf, final TopologyContext context) {
 //		locatorService = new GoogleLocatorService();
 		locatorService = new RedisLocatorService();
 	}
 
 	@Override
-	public void execute(Tuple tuple, BasicOutputCollector outputCollector) {
+	public void execute(final Tuple tuple, final BasicOutputCollector outputCollector) {
 		logger.debug("Got:"+tuple);
-		String str = tuple.getStringByField("str");
-		String[] parts = str.split("@");
-		Long time = Long.valueOf(parts[0]);
-		String address = parts[1];
+		final String str = tuple.getStringByField("str");
+		final String[] parts = str.split("@");
+		final Long time;
+		try{
+			time = Long.valueOf(parts[0]);
+		} catch(final NumberFormatException ex){
+			logger.error(parts[0] +"-"+ex.getMessage());
+			return;
+		}
+		final String address = parts[1];
 		
-		LocationDTO locationDTO = locatorService.getLocation(address);
+		final LocationDTO locationDTO = locatorService.getLocation(address);
 		if(locationDTO!=null) {
-			Values emitTuple = new Values(locationDTO.getCity(),time,locationDTO);
+			final Values emitTuple = new Values(locationDTO.getCity(),time,locationDTO);
 			logger.debug("Emit:"+emitTuple);
 			outputCollector.emit(emitTuple );
 		}
