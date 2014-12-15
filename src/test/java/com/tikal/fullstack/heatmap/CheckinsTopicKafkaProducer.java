c827a6be385e8586efedffb716b55ffdfa36b2d8
Yanai Franchi <yanai@tikalk.com>
2014-12-15 09:08:49 +0200

    change to main

--- src/test/java/com/tikal/fullstack/heatmap/CheckinsTopicKafkaProducer.java
+++ src/test/java/com/tikal/fullstack/heatmap/CheckinsTopicKafkaProducer.java
@@ -8,6 +8,7 @@ import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
 
 public class CheckinsTopicKafkaProducer {
+	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(CheckinsTopicKafkaProducer.class);
 	private static final String sampleAddress = "@ADDRESS:1600+Amphitheatre+Parkway,+Mountain+View,+CA";
 	private static final String serClass = "kafka.serializer.StringEncoder";
 	private static final String zkHosts = "localhost:9092";
@@ -16,8 +17,11 @@ public class CheckinsTopicKafkaProducer {
 	public static void main(final String[] args) throws InterruptedException {
 		final Producer<String, String> producer = createProducer();
 		
-		for(;true;Thread.sleep(1000))
-            producer.send(new KeyedMessage<String, String>(topicName,new Date().getTime()+sampleAddress));        
+		for(;true;Thread.sleep(1000)){
+			final String msg = new Date().getTime()+sampleAddress;
+			logger.debug("Sending {}...",msg);
+            producer.send(new KeyedMessage<String, String>(topicName,msg));
+		}
 
 //		producer.close();
 	}
